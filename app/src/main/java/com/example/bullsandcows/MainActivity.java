4e81a9c624bcd25ecfc8cde11d726fe321096a63
1Gregorio1 <67803765+1Gregorio1@users.noreply.github.com>
2022-07-30 23:16:39 +0300

    Second commit

--- app/src/main/java/com/example/bullsandcows/MainActivity.java
+++ app/src/main/java/com/example/bullsandcows/MainActivity.java
@@ -7,13 +7,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.awt.font.TextAttribute;
 
 import static java.lang.System.out;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    TextView textBulls, textCows, textView4, textView5, textView6;
+    Button ok, regulations, link;
+    TextView textBulls, textCows, amountBulls, amountCows, mainText;
     EditText editText;
 
 
@@ -21,19 +24,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
+        ok = findViewById(R.id.ok);
+        regulations = findViewById(R.id.regulations);
+        link = findViewById(R.id.link);
         textBulls = findViewById(R.id.textBulls);
         textCows = findViewById(R.id.textCows);
-        textView4 = findViewById(R.id.textView4);
-        textView5 = findViewById(R.id.textView5);
-        textView6 = findViewById(R.id.textView6);
+        amountBulls = findViewById(R.id.amountBulls);
+        amountCows = findViewById(R.id.amountCows);
+        mainText = findViewById(R.id.mainText);
         editText = findViewById(R.id.editText);
 
 
         String secretNumber = makeNumber();
-        textView6.setText("Угадайте число из 4 уникальных цифр");
+        mainText.setText("Угадайте число из 4 уникальных цифр");
         //textView6.setTextColor(#FF000000);
-        button.setOnClickListener(new View.OnClickListener() {
+        ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String line = editText.getText().toString();
@@ -42,20 +47,20 @@ public class MainActivity extends AppCompatActivity {
                 if (isNumeric(line) && line.length() == 4) {
                     String result = searchForMatches(secretNumber, line);
 
-                    textView4.setText(result.charAt(0) + "");
-                    textView5.setText(result.charAt(2) + "");
-                    textView6.setText("Прошлая комбинация: " + line);
+                    amountBulls.setText(result.charAt(0) + "");
+                    amountCows.setText(result.charAt(2) + "");
+                    mainText.setText("Прошлая комбинация: " + line);
                     if (line.equals("0000")) {
-                        textView6.setText("Загаданное число: " + secretNumber);
+                        mainText.setText("Загаданное число: " + secretNumber);
                     }
                     editText.setText("");
 
                     if (result.charAt(0) == '4') {
-                        textView6.setText("Вы победили. Загаданое число: " + secretNumber);
-                        button.setEnabled(false);
+                        mainText.setText("Вы победили. Загаданое число: " + secretNumber);
+                        ok.setEnabled(false);
                     }
                 } else {
-                    textView6.setText("Введите значение из 4 цифр");
+                    mainText.setText("Введите значение из 4 цифр");
                 }
 
 
@@ -63,6 +68,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        regulations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createButtonAlertDialog("Правила","Компьютер задумывает четыре различные цифры из 0,1,2,...9. \n" +
+                        "Игрок делает ходы, чтобы узнать эти цифры и их порядок.\n" +
+                        "\n" +
+                        "Каждый ход состоит из четырёх цифр. \n" +
+                        "\n" +
+                        "В ответ компьютер показывает число отгаданных цифр, стоящих на своих местах (число быков)\n" +
+                        " и число отгаданных цифр, стоящих не на своих местах (число коров).");
+
+            }
+        });
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                createButtonAlertDialog("Ссылка на GitHub","https://github.com/1Gregorio1/BullsAndCows");
+
+            }
+        });
+
+
+    }
+
+    private void createButtonAlertDialog(String title, String content){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title);
+        builder.setMessage(content);
+        builder.show();
     }
 
 
